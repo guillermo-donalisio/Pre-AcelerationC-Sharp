d54b122596ea070da36cca7b8c792caa994180f6
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-05 12:59:01 -0300

    DB config and string

--- Api-Disney/Data/DisneyContext.cs
+++ Api-Disney/Data/DisneyContext.cs
@@ -1,8 +1,35 @@
+using Api_Disney.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api_Disney.Data;
 
 public class DisneyContext : DbContext
 {
+    public DisneyContext(DbContextOptions<DisneyContext> options): base(options)
+    {  
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    public DbSet<Character>? Characters {set;get;}
+    public DbSet<MovieCharacter>? MoviesCharacters {set;get;}
+    public DbSet<Movie>? Movies {set;get;}
+    public DbSet<GenreMovie>? GenresMovies {set;get;}
+    public DbSet<Genre>? Genres {set;get;}
+
+
     
 }
--- Api-Disney/Program.cs
+++ Api-Disney/Program.cs
@@ -1,6 +1,13 @@
+using Api_Disney.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DisneyContext>(x => x.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
